d1637a6e2c7f935607b22e69ba7e057399e3bba7
belozi <robert.belozi.lee@gmail.com>
2014-11-17 06:07:44 -0500

    My first Java programs

---
+++ AlarmClockTest.java
@@ -0,0 +1,8 @@
+public class AlarmClockTest {
+    public static void 
+main(String[] args) {
+	  AlarmClock aClock = new 
+AlarmClock();
+	aClock.snooze();
+	}
+}
---
+++ MyProgram.java
@@ -0,0 +1,7 @@
+public class MyProgram{
+
+  public static void main(String[] args) {
+    System.out.println(
+	  "Eureka, I can put Java on my resume.");
+  }
+}  
